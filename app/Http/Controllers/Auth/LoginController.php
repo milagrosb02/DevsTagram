e1eaa6092c9a779543d24681f0bd917bbf467517
milagrosb02 <arimibor@hotmail.com>
2023-10-23 21:27:19 -0300

    Login hecho

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,5 +7,36 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+
+            'email' => ['required', 'email'],
+
+            'password' => ['required'],
+
+        ]);
+
+
+        // comprueba si las credenciales son correctas
+        // esto retorna true o false
+        if(!auth()->attempt($request->only('email','password')))
+        {   
+            // este mensaje se muestra en la vista y es creado con with
+            // back() hace que vuelva a la pagina anterior
+            return back()->with('mensaje', 'Las credenciales no son correctas. '); 
+        }
+
+        // si los datos fueron correctos
+        return redirect()->route('posts.index');
+
+    }
+
+
 }
