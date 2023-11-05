c37eda7e3c89ceb7817675b8ccdc71980856de78
milagrosb02 <arimibor@hotmail.com>
2023-11-05 12:21:53 -0300

    Imagen subida correctamente

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
@@ -12,7 +14,26 @@ class ImagenController extends Controller
         // De esta manera identifico el archivo que subo
         $imagen = $request->file('file');
 
-        return response()->json(['imagen' => $imagen -> extension()]);
+
+        // Variable para generar ids de archivos unicos
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+
+        // Variable para hacer uso de intervetion image
+        $imagenServidor = Image::make($imagen);
+
+        // Hago uso de un efecto de intervetion image
+        $imagenServidor->fit(1000,1000);
+
+        // Variable para almacenar la imagen en el servidor
+        // lugar donde se va a guardar la foto 
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+
+        // Guardo imagen en el servidor
+        $imagenServidor->save($imagenPath);
+
+
+        return response()->json(['imagen' => $nombreImagen]);
 
     }
 
